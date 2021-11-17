a81265d78c64ba4259b9312cd290b9ab3d787299
Diounata <jonathamcordeiro42@gmail.com>
2021-11-17 14:35:25 -0300

    Add: Page title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import styles from '@styles/index.module.scss';
 
+import Title from 'utils/Title';
 import Header from 'Components/Header';
 import Result from 'Components/Result';
 import Keypad from 'Components/Keypad';
@@ -7,6 +8,8 @@ import Keypad from 'Components/Keypad';
 export default function Home() {
   return (
     <div className={`dark-theme ${styles.container}`}>
+      <Title />
+
       <Header />
       <Result />
       <Keypad />
---
+++ src/utils/Title.tsx
@@ -0,0 +1,9 @@
+import Head from 'next/head';
+
+export default function Title() {
+  return (
+    <Head>
+      <title>Calculator.io</title>
+    </Head>
+  );
+}
