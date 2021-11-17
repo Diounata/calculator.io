88ddc890dbd1d976f770da61adc08df1a9566a5f
Diounata <jonathamcordeiro42@gmail.com>
2021-11-16 22:16:38 -0300

    Add: header component

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,5 +1,19 @@
 import styles from './styles.module.scss';
 
-export function Header() {
-  return <header></header>;
+export default function Header() {
+  return (
+    <header className={styles.headerContainer}>
+      <h3>calc</h3>
+
+      <div className={styles.themeContainer}>
+        <h3>THEME</h3>
+
+        <div>
+          <div className={styles.selected}></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+    </header>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import styles from '@styles/index.module.scss';
 
+import Header from 'Components/Header';
 import Result from 'Components/Result';
 
 export default function Home() {
   return (
     <div className={`dark-theme ${styles.container}`}>
+      <Header />
       <Result />
     </div>
   );
